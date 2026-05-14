918cf52a283de12b97c6995ec3223c354f2e6499
Caio <caio@teste.com>
2026-05-14 19:28:26 -0300

    Add: Viewsets

--- product/serializers/product_serializers.py
+++ product/serializers/product_serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
+from product.models.category import Category
 from product.models.product import Product
 from product.serializers.category_serializers import CategorySerializer
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(required=True, many=True)
+    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.ojects.all(), write_only=True, many=True)
 
     class Meta:
         model = Product
@@ -14,4 +16,14 @@ class ProductSerializer(serializers.ModelSerializer):
             'price',
             'active',
             'category',
+            'category_id',
         ]
+
+    def create(self, validated_data):
+        category_data = validated_data.pop('categories_id')
+
+        product = Product.objects.create(**validated_data)
+        for category in category_data:
+            product.category.add(category)
+
+        return product
